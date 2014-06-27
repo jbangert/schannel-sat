4c8c31ac31f1bf8c6b99f891ebb2a8cdb2d39490
Julian Bangert <jbangert@acm.org>
2014-06-27 18:57:12 -0400

    RSA demo

--- demo/rsa.c
+++ demo/rsa.c
@@ -8,30 +8,43 @@ int fp_print(fp_int *test){
         printf("%s\n",buf);
         return 0;
 }
+static void scatter(fp_digit table[], int choice, fp_int *foo,int width){
+        int i = 0;
+        for(i=0;i<width;i++)
+                table[(1<<TABLE) * i + choice] = foo->dp[i];
+}
+static void gather(fp_digit table[], int choice, fp_int *foo, int width){
+        int i = 0;
+        for(i=0;i<width;i++)
+                foo->dp[i]= table[(1<<TABLE) * i + choice];
+        foo->used = width;
+        foo->sign = 0;
+}
 //WE need a compile time assert for byte-width
-int table_exp(fp_int *a, fp_int *b,fp_int *m, fp_int *res){
-        fp_int table[1<<TABLE];   
-        fp_int temp,tempb, tempc;
+int table_sc_exp(fp_int *a, fp_int *b,fp_int *m, fp_int *res){
+        fp_digit table[FP_SIZE * (1<<TABLE)];   
+        fp_int temp, temp1;
         fp_digit buf,mp,y;
         int i,digidx,err, bitcnt;
+        int width = a->used;
         if ((err = fp_montgomery_setup (m, &mp)) != FP_OKAY) {
                 return err;
         }
         /* now we need R mod m */
         fp_montgomery_calc_normalization (&temp, m); //Set used to 16 everywhere!
         /* now set R[0][1] to G * R mod m */
-        fp_mod(a, m, &tempb);
-        fp_mulmod (&tempb, &temp, m, &tempb); 
+        fp_mod(a, m, &temp1);
+        fp_mulmod (&temp1, &temp, m, &temp1); 
         //Table[0] = Mont(1) = temp
-        //Table[1] = Mont(a) = tempb
-        fp_init_copy(&table[0], &temp);
-        fp_init_copy(&table[1], &tempb);
-        fp_init_copy(&tempc, &tempb);
+        //Table[1] = Mont(a) = temp1
+        scatter(table,0, &temp, width);
+        fp_init_copy(&temp, &temp1);
+        scatter(table,1, &temp1,width);
+
         for(i=2;i<(1<<TABLE); i++){
-                fp_mul(&tempb,&tempc,&temp);
+                fp_mul(&temp,&temp1,&temp);
                 fp_montgomery_reduce(&temp, m, mp);
-                fp_init_copy(&tempb,&temp);
-                fp_init_copy(&table[i], &temp);
+                scatter(table,i,&temp,width);
         }
         digidx= b->used-1;
         buf=b->dp[digidx--];
@@ -39,7 +52,7 @@ int table_exp(fp_int *a, fp_int *b,fp_int *m, fp_int *res){
 
         y = (buf >> (DIGIT_BIT-TABLE)) & ((1<<TABLE) -1);
         buf<<=(fp_digit)TABLE;
-        fp_init_copy(res,&table[y]);
+        gather(table,y,res,width);
         for(;;){
                 bitcnt -= TABLE;
                 if(bitcnt==0 ){
@@ -55,13 +68,13 @@ int table_exp(fp_int *a, fp_int *b,fp_int *m, fp_int *res){
                         fp_sqr(res,res);                  
                         fp_montgomery_reduce(res,m, mp);
                 }
-                fp_mul(res, &table[(int)y], res );
+                gather(table,y,&temp,width);
+                fp_mul(res, &temp, res );
                 fp_montgomery_reduce(res,m, mp);
         }
         fp_montgomery_reduce(res,m,mp);
         return 0;
 }
-#if 0
 int table_exp(fp_int *a, fp_int *b,fp_int *m, fp_int *res){
         fp_int table[1<<TABLE];   
         fp_digit buf,mp,y;
@@ -108,8 +121,26 @@ int table_exp(fp_int *a, fp_int *b,fp_int *m, fp_int *res){
         fp_montgomery_reduce(res,m,mp);
         return 0;
 }
-#endif
-int main(void)
+int fasttest(void){
+
+   fp_int d, e, n, c, m, e_m;
+   /* read in the parameters */
+   fp_read_radix(&n, "a7f30e2e04d31acc6936916af1e404a4007adfb9e97864de28d1c7ba3034633bee2cd9d5da3ea3cdcdc9a6f3daf5702ef750f4c3aadb0e27410ac04532176795995148cdb4691bd09a8a846e3e24e073ce2f89b34dfeb2ee89b646923ca60ee3f73c4d5397478380425e7260f75dfdc54826e160395b0889b1162cf115a9773f", 16);
+   fp_read_radix(&d, "16d166f3c9a404d810d3611e6e8ed43293fe1db75c8906eb4810785a4b82529929dade1db7f11ac0335d5a59773e3167b022479eedefa514a0399db5c900750a56323cf9f5b0f21e7d60a46d75f3fcaabf30a63cbe34048b741a57ac36a13914afda798709dea5771f8d456cf72ec5f3afc1d88d023de40311143a36e7028739", 16);
+   fp_read_radix(&c, "7d216641c32543f5b8428bdd0b11d819cfbdb16f1df285247f677aa4d44de62ab064f4a0d060ec99cb94aa398113a4317f2c550d0371140b0fd2c88886cac771812e72faad4b7adf495b9b850b142ccd7f45c0a27f164c8c7731731c0015f69d0241812e769d961054618aeb9e8e8989dba95714a2cf56c9e525c5e34b5812dd", 16);
+   fp_read_radix(&m, "5f323bf0b394b98ffd78727dc9883bb4f42287def6b60fa2a964b2510bc55d61357bf5a6883d2982b268810f8fef116d3ae68ebb41fd10d65a0af4bec0530eb369f37c14b55c3be60223b582372fb6589b648d5a0c7252d1ae2dae5809785d993e9e5d0c4d9b0bcba0cde0d6671734747fba5483c735e1dab7df7b10ec6f62d8", 16);
+
+   clock_t t1;
+   int x;
+   t1 = clock();
+   for (x = 0; x < 1000; x++) {
+      table_sc_exp(&c, &d, &n, &e_m);
+   }
+   t1 = clock() - t1;
+   printf("1000 sctable operations took      %10.5g seconds\n", (double)t1 / (double)CLOCKS_PER_SEC);
+   printf("RSA (sctable) decrypt/sec              %10.5g\n", (double)CLOCKS_PER_SEC / ((double)t1 / 1000.0) );
+}
+int test(void)
 {
    fp_int d, e, n, c, m, e_m;
    clock_t t1;
@@ -190,6 +221,13 @@ int main(void)
    printf("100 table operations took      %10.5g seconds\n", (double)t1 / (double)CLOCKS_PER_SEC);
    printf("RSA (table) decrypt/sec              %10.5g\n", (double)CLOCKS_PER_SEC / ((double)t1 / 100.0) );
 
+   t1 = clock();
+   for (x = 0; x < 100; x++) {
+      table_sc_exp(&c, &d, &n, &e_m);
+   }
+   t1 = clock() - t1;
+   printf("100 sctable operations took      %10.5g seconds\n", (double)t1 / (double)CLOCKS_PER_SEC);
+   printf("RSA (sctable) decrypt/sec              %10.5g\n", (double)CLOCKS_PER_SEC / ((double)t1 / 100.0) );
 
    /* test half size */
    fp_rshd(&n, n.used >> 1);
@@ -212,6 +250,9 @@ int main(void)
    return 0;
 }
 
+int main(void){
+        test();
+}
 /* $Source: /cvs/libtom/tomsfastmath/demo/rsa.c,v $ */
 /* $Revision: 1.2 $ */
 /* $Date: 2005/05/05 14:39:33 $ */
